75f1a5d85bf66af26cff9c97c77bc5ac169bf498
denisrang <d.rangulov@innopolis.ru>
2018-11-17 23:20:45 +0300

    Initial commit

---
+++ app/src/main/java/course/inno/aws/enlightning/utils/CognitoConstants.java
@@ -0,0 +1,12 @@
+package course.inno.aws.enlightning.utils;
+
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+
+public class CognitoConstants {
+
+    public static final String USER_POOL_ID = "us-east-1_MSqq41udM";
+    public static final String CLIENT_ID = "6dd1imgakntvvl7a03m45pggg5";
+    public static final String CLIENT_SECRET = "kgecs5f65no2n2r556h21gjfje6gndj7d535pchprl22h5biuo9";
+    public static final Regions COGNITO_REGION = Regions.US_EAST_1;
+}
